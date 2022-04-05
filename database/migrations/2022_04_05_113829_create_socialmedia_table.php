ae83dbd564bda32bb0314421511cebb11b9e848f
omer <omeratayilmaz@gmail.com>
2022-04-05 23:34:29 +0300

    creating all models

---
+++ database/migrations/2022_04_05_113829_create_socialmedia_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialmediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socialmedia', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name',255);
+            $table->string('image',255);
+            $table->string('description',255);
+            $table->string('title',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socialmedia');
+    }
+}
---
+++ database/migrations/2022_04_05_113858_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->text('comment');
+            /* float value:rate */
+            $table->string('news_id');
+            $table->string('user_id');
+            $table->string('ip',255);
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
